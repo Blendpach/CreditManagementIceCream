169271c099094538e09809f86d7fa6b32df2ddc3
Dinith Shavidu <dinithshavidu56@gmail.com>
2022-02-11 01:35:12 +0530

    sssssssss

--- application/models/Setups.php
+++ application/models/Setups.php
@@ -365,15 +365,15 @@ class Setups extends CI_Model
         $pro_name = "";
         $pro_sku = "";
         $pro_desc = "";
-        $pro_cid = "";
-        $pro_date = "";
+       // $pro_cid = "";
+       // $pro_date = "";
        // $pro_active = 0;
 
         $pro_id = $this->input->post('pro_id');
         $pro_name = $this->input->post('pro_name');
         $pro_sku = $this->input->post('pro_sku');
         $pro_desc = $this->input->post('pro_desc');
-        $pro_cid = $this->session->userdata('passed_user_national');
+        //$pro_cid = $this->session->userdata('passed_user_national');
        // $pro_date = $this->input->post('pro_date');
         // $pro_active = $this->input->post('pro_active');
 
@@ -385,7 +385,7 @@ class Setups extends CI_Model
             'pro_name' => $pro_name,
             'pro_sku' => $pro_sku,
             'pro_desc' => $pro_desc,
-            'pro_cid' => $pro_cid,
+            //'pro_cid' => $pro_cid,
             //'pro_date' => $pro_date,
            // 'pro_active' => $pro_active,
 
