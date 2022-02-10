1abfdbac1c8f42ca322724009278411b5a68b617
BlendPach <blendpach@gmail.com>
2022-02-11 01:50:12 +0530

    453

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
  */
-$config['base_url'] = 'http://localhost/CreditManagementIceCream'; 
+$config['base_url'] = 'https://pro2.blendpach.host';
 
 /*
 |--------------------------------------------------------------------------
